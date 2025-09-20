a3df00908ce339ae8e913d29e968ab6ef59d71e5
hawra <hawra.yusuf02@outlook.com>
2025-09-20 22:07:08 +0300

    created foods controller and imported

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const passUserToView = require("./middleware/pass-user-to-view.js");
 
 // Controllers
 const authController = require('./controllers/auth.js');
+const foodsController = require('./controllers/foods.js');
 
 // Set the port from environment variable or default to 3000
 const port = process.env.PORT ? process.env.PORT : '3000';
@@ -51,6 +52,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/auth', authController);
+app.use('/users/:userId/foods', foodsController);
 
 // PROTECTED
 
