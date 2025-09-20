0c49e968e9b7238832b6104f60d207b56c893b95
hawra <hawra.yusuf02@outlook.com>
2025-09-20 22:01:01 +0300

    embedded food schema

--- models/user.js
+++ models/user.js
@@ -1,5 +1,29 @@
 const mongoose = require('mongoose');
 
+const foodSchema = new mongoose.Schema({
+  name:{
+    type: String,
+    required: true,
+  },
+  quantity:{
+    type: Number,
+    required: true,
+    default: 1,
+  },
+  unit:{
+    type: String,
+    enum: ['kg', 'g', 'lbs', 'oz'],
+    default: 'kg',
+  },
+  expiration:{
+    type: Date,
+    required: true,
+  },
+  notes:{
+    type: String,
+  },
+});
+
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
@@ -9,6 +33,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  pantry: [foodSchema],
 });
 
 const User = mongoose.model('User', userSchema);
